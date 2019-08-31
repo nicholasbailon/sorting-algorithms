e8c48af4c41e2a3456161b031d4ea3e1a54eff2e
Nicholas Bailon <nikobaca@gmail.com>
2019-08-31 18:39:10 +0200

    Initial commit. Created a boilerplate project using React. Also did some file cleaning.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
